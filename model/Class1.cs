04025ab5842bfdb5f5c9696f7a8d1342cd3e9702
Felipe Sánchez <vegafelipe015@gmail.com>
2020-01-23 10:15:23 -0500

    Starting with the model

---
+++ model/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace model
+{
+	public class DataManager{
+
+		private string name;
+		private string code;
+		private string room;
+		private TimeSpan startTime;
+		private TimeSpan endTime;
+
+		public string Name {
+
+			get {
+				return name;
+			}
+
+			set {
+				name = value;
+			}
+		}
+
+		public string Code
+		{
+			get
+			{
+				return code;
+			}
+
+			set
+			{
+				code = value;
+			}
+		}
+
+		public string Room
+		{
+
+			get
+			{
+				return room;
+			}
+
+			set
+			{
+				room = value;
+			}
+		}
+
+		public TimeSpan StartTime
+		{
+			get
+			{
+				return startTime;
+			}
+
+			set
+			{
+				startTime = value;
+			}
+		}
+
+		public TimeSpan EndTime
+		{
+			get
+			{
+				return endTime;
+			}
+
+			set
+			{
+				endTime = value;
+			}
+		}
+
+	}
+}
